a37ae31086cf5a94d16fbbd820f4ed9c4b189677
Roman <mazaxaka.tyt@gmail.com>
2022-01-19 16:40:13 +0200

    add new slides

---
+++ app.js
@@ -0,0 +1,22 @@
+$('.scrollto a').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 500,   // по умолчанию «400» 
+        easing: "linear" // по умолчанию «swing» 
+    });
+
+    return false;
+});
+
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        dots: true,
+        autoplaySpeed: 5000,
+        adaptiveHeight: true,
+    });
+  });
